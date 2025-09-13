50695f588f3d4e4ddbcb57afd751c2532ec02acc
bqerzabyte <bqerzalaonline@gmail.com>
2025-09-13 05:32:32 +0300

    Add files via upload

---
+++ save-token.php
@@ -0,0 +1,1 @@
+<?php$data = json_decode(file_get_contents('php://input'), true);$token = $data['token'] ?? '';if($token) {    file_put_contents('tokens.txt', $token . PHP_EOL, FILE_APPEND);    echo json_encode(['status' => 'success']);} else {    echo json_encode(['status' => 'error', 'message' => 'No token received']);}?>
